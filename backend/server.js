d4b19a6d580c2a4f2a529ff8e829efd346a3d3a0
Muhammad Zulqarnain <zulqarnain.fastian@gmail.com>
2024-04-03 22:40:35 +0500

    Archiving of invoices

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ dotenv.config();
 const connectDB = require("./config/db.js");
 const app = require("./app");
 const { addAllDriversNotifications } = require("./services/driverService.js");
-const { archiveInvoices } = require("./services/invoiceService.js");
+const {
+  archiveDriverInvoices,
+  archiveEmployeeInvoices,
+  archivePettyCashInvoices,
+} = require("./services/invoiceService.js");
 
 connectDB();
 
@@ -13,5 +17,11 @@ app.listen(port, () => console.log(`Server started on port ${port}`));
 // Running this function every day to update driver documents expiry notifications
 setInterval(addAllDriversNotifications, 24 * 60 * 60 * 1000);
 
-// Running this function every day to update invoices
-setInterval(archiveInvoices, 24 * 60 * 60 * 1000);
+// Running this function every day to update driver invoices on 1st of every month
+setInterval(archiveDriverInvoices, 24 * 60 * 60 * 1000);
+
+// Running this function every day to update driver invoices on 1st of every month
+setInterval(archiveEmployeeInvoices, 24 * 60 * 60 * 1000);
+
+// Running this function every day to update petty cash invoices on 1st of every month
+setInterval(archivePettyCashInvoices, 24 * 60 * 60 * 1000);
--- backend/services/invoiceService.js
+++ backend/services/invoiceService.js
@@ -1,6 +1,8 @@
 const DriverInvoice = require("../models/driverInvoiceModel");
+const EmployeeInvoice = require("../models/employeeInvoiceModel");
+const PettyCash = require("../models/pettyCashModel");
 
-const archiveInvoices = async () => {
+const archiveDriverInvoices = async () => {
   try {
     const currentDate = new Date();
 
@@ -16,10 +18,58 @@ const archiveInvoices = async () => {
       await invoice.save();
     }
 
-    console.log("Invoices updated successfully");
+    console.log("Driver Invoices updated successfully");
   } catch (error) {
-    console.error("Error updating invoices:", error);
+    console.error("Error updating driver invoices:", error);
   }
 };
 
-module.exports = { archiveInvoices };
+const archiveEmployeeInvoices = async () => {
+  try {
+    const currentDate = new Date();
+
+    if (currentDate.getDate() !== 1) return;
+
+    const activeInvoices = await EmployeeInvoice.find({
+      status: { $in: ["pending", "approved"] },
+    });
+
+    for (const invoice of activeInvoices) {
+      invoice.status = "archived";
+
+      await invoice.save();
+    }
+
+    console.log("Employee invoices updated successfully");
+  } catch (error) {
+    console.error("Error updating employee invoices:", error);
+  }
+};
+
+const archivePettyCashInvoices = async () => {
+  try {
+    const currentDate = new Date();
+
+    if (currentDate.getDate() !== 1) return;
+
+    const activeInvoices = await PettyCash.find({
+      status: { $in: ["pending", "approved"] },
+    });
+
+    for (const invoice of activeInvoices) {
+      invoice.status = "archived";
+
+      await invoice.save();
+    }
+
+    console.log("Petty cash invoices updated successfully");
+  } catch (error) {
+    console.error("Error updating petty cash invoices:", error);
+  }
+};
+
+module.exports = {
+  archiveDriverInvoices,
+  archiveEmployeeInvoices,
+  archivePettyCashInvoices,
+};
